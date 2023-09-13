9c14a00cca3290792196573774fd6c397328afc2
Robert Dico <114947225+eljefe1111@users.noreply.github.com>
2023-09-13 14:00:27 -0400

    Add files via upload

---
+++ Project1.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.print("T");
+    System.out.println("Tr");
+    System.out.println("Tri");
+    System.out.println("Tria");
+    System.out.println("Trian");
+    System.out.println("Triang");
+    System.out.println("Triangl");
+    System.out.println("Triangle");
+   
+    System.out.println("SQUARE");
+    System.out.println("Q    R");
+    System.out.println("U    A");
+    System.out.println("A    U");
+    System.out.println("R    Q");
+    System.out.println("ERAUQS");
+    
+    
+  }
+}

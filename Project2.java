335574b9f21b22890170e403b0f00c68015eba22
Robert Dico <114947225+eljefe1111@users.noreply.github.com>
2023-09-13 14:00:58 -0400

    Add files via upload

---
+++ Project2.java
@@ -0,0 +1,15 @@
+//Program to add 2 numbers
+class Main {
+  public static void main(String[] args)
+  {
+int a;
+int b;
+a = 50;
+b = 33;
+println (a+b);
+println (a-b);
+println (a*b);
+println (a/b);
+println (a%b);
+  }
+}

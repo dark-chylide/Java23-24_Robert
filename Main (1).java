427505cd128113c47571a14fd3e484c219ce71ca
Robert Dico <114947225+eljefe1111@users.noreply.github.com>
2023-09-15 13:30:32 -0400

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+ double radius = 5.67;
+    double pi = 3.14;
+    double area;
+  
+     area= pi*radius*radius;
+    System.out.println ("Area is "+area);
+   
+  }
+}

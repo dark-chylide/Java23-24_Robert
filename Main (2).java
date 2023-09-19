d84c388e93338d4ecd5efea432852fea61858990
Robert Dico <114947225+eljefe1111@users.noreply.github.com>
2023-09-19 13:23:57 -0400

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,19 @@
+class Main {
+  public static void main(String[] args) {
+    int a,b;
+    a = 7;
+    b = 3;
+    //int x = a % b;
+    //System.out.println(x);
+    int x = a / b;
+    double y = a / b;
+   // System.out.println("Integer value is "+x);
+  // System.out.println("Double value is "+y);
+     System.out.println(a>b);
+     System.out.println(a<b);
+     System.out.println(a==b);
+     System.out.println(a!=b);
+     System.out.println(a>=b);
+     System.out.println(a<=b);
+  }
+}

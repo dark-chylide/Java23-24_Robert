b52ea0ef0296dffd4c41a3116c9d32c47ce4bc50
Robert Dico <114947225+eljefe1111@users.noreply.github.com>
2023-09-27 13:02:20 -0400

    Add files via upload

---
+++ Main (6).java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    int a = 7;
+      System.out.println("addition " + (a+=1));
+      System.out.println("Subtraction " + (a-=1));
+      System.out.println("Multiplication " + (a*=1));
+      System.out.println("Division " + (a/=1));
+  System.out.println("____________________");
+    int i = 3; 
+    int j = 6;
+    if (i>j)
+    {
+      System.out.println("i is greater");
+    }
+    else
+    {
+      System.out.println("j is greater");
+    }
+  }
+}

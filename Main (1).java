2521b76267b0b45313f9099a7d48395a591b57db
Robert Dico <114947225+eljefe1111@users.noreply.github.com>
2023-09-29 13:47:55 -0400

    Update Main (1).java

--- Main (1).java
+++ Main (1).java
@@ -1,7 +1,7 @@
 class Main {
   public static void main(String[] args) {
  double radius = 5.67;
-    double pi = 3.14;
+   final double pi = 3.14;
     double area;
   
      area= pi*radius*radius;

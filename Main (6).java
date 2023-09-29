1ca0345c9a3d5898d23f012736628a86757012f5
Robert Dico <114947225+eljefe1111@users.noreply.github.com>
2023-09-29 12:44:10 -0400

    Update Main (6).java

--- Main (6).java
+++ Main (6).java
@@ -1,11 +1,13 @@
 class Main {
   public static void main(String[] args) {
+    //short arithmetic operators
     int a = 7;
       System.out.println("addition " + (a+=1));
       System.out.println("Subtraction " + (a-=1));
       System.out.println("Multiplication " + (a*=1));
       System.out.println("Division " + (a/=1));
   System.out.println("____________________");
+   //what is greater
     int i = 3; 
     int j = 6;
     if (i>j)

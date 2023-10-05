c29395eee430c5e730e5b429232023fb8ff1bbf0
Robert Dico <114947225+eljefe1111@users.noreply.github.com>
2023-10-05 13:57:27 -0400

    Update ScannerExamples.java

--- ScannerExamples.java
+++ ScannerExamples.java
@@ -2,12 +2,15 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
+      
         System.out.println("Enter two numbers");
 
-    int num1 = input.nextInt();
-    int num2 = input.nextInt();
-
-    System.out.println("Number is "+ (num1+num2));
+   // string = input.nextLine();
+   // int num1 = input.nextInt();
+   // int num2 = input.nextInt();
+      double dbl1 = input.nextDouble();
+      double dbl2 = input.nextDouble();
+    System.out.println("Double is "+ (dbl1+dbl2));
 
     input.close();
   }

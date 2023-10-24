6a4d2cced48c2174feb3b77f0d5aad2d7902ca4b
Robert Dico <114947225+eljefe1111@users.noreply.github.com>
2023-10-24 13:54:54 -0400

    Add files via upload

---
+++ switchcase (1).java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    int x = 87;
+    switch (x) 
+      {
+      case 90:
+        System.out.println("A");
+          break;
+      case 80:
+        System.out.println("B");
+          break;
+      case 70:
+        System.out.println("C");
+      }
+  }
+}

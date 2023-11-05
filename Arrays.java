b326fe4435d9a1379da2daf950d1880a20ef93e3
Robert Dico <114947225+dark-chylide@users.noreply.github.com>
2023-11-05 16:36:50 -0500

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -10,5 +10,15 @@ class Main {
       }
     }
     System.out.println("The biggest number is " + max);
+    //-----------------------
+    int ar[] = {1, 2, 3, 4, 5, 8 , 10, 89, 101};
+      System.out.println(ar.length);
+
+    System.out.println("_________________-");
+    String [] name = {"Alexander","Asees", "Adam" };  
+  for(int i=0; i<name.length; i++)
+    {
+      System.out.println(name[i]);
+    }
   }
 }

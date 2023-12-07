18b841fc001dd315674839a22be7b03568d8f10b
Robert Dico <114947225+dark-chylide@users.noreply.github.com>
2023-12-07 14:14:15 -0500

    Add files via upload

---
+++ login-info_Robert/src/main/java/com/mycompany/login/info_robert/LoginInfo_Robert.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.login.info_robert;
+
+/**
+ *
+ * @author robert
+ */
+public class LoginInfo_Robert {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
